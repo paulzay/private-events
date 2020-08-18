b09d87d300b301071f1a97b5cc6dbe5519638ed9
paulzay <paulotieno2@gmail.com>
2020-08-18 13:27:08 +0300

    implement sessions feature

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,25 +1,26 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:show]
+  def show
+    @user = User.find(params[:id])
+  end
 
-	def show
-		@user = User.find(params[:id])
-	end
+  def new
+    @user = User.new
+  end
 
-	def new
-		@user = User.new
-	end
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      log_in @user
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
 
-	def create
-		@user = User.new(user_params)
-		if @user.save 
-			redirect_to @user
-		else
-			render :new
-		end
-	end
+  private
 
-	private
-
-	def user_params
-		params.require(:user).permit(:name, :email)
-	end 
+  def user_params
+    params.require(:user).permit(:name, :email)
+  end
 end
