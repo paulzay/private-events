45cf331cd72b97c8cbd1b7f49e6df0ad0dd0ff55
paulzay <paulotieno2@gmail.com>
2020-08-20 12:50:24 +0300

    Remodify project to fix issues
    Drop the database and migrations.
    Modify the migration files to project specs
    Add association between user and events

--- db/migrate/20200818122002_create_events.rb
+++ db/migrate/20200818122002_create_events.rb
@@ -1,8 +1,12 @@
 class CreateEvents < ActiveRecord::Migration[5.2]
   def change
     create_table :events do |t|
-     
-      t.date :event_date
+      t.integer :creator_id
+      t.string :name
+      t.string :date
+      t.string :description
+      t.string :location
+
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,20 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_18_130053) do
+ActiveRecord::Schema.define(version: 2020_08_18_122002) do
 
   create_table "events", force: :cascade do |t|
-    t.date "event_date"
+    t.integer "creator_id"
+    t.string "name"
+    t.string "date"
+    t.string "description"
+    t.string "location"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "user_id"
-    t.string "description"
-    t.index ["user_id"], name: "index_events_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
-    t.string "emal"
+    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
